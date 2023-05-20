795db73a3b00aa466e911d9442eb36dbe9122eea
Carlos Aumente <carlosaumente@gmail.com>
2023-05-21 01:18:51 +0200

    Training CV without validation file refactored

--- src/training_segmentation_CV_prod.py
+++ src/training_segmentation_CV_prod.py
@@ -1,18 +1,18 @@
 import logging
 import shutil
+import sys
 import time
 from datetime import datetime
 from pathlib import Path
 from pprint import pformat
-import sys
 
 import numpy as np
 import torch
 import yaml
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from torchvision import transforms
 
-from src.dataset.BUSI_dataloader import BUSI_dataloader, BUSI_dataloader_CV_prod
-from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingLR
+from src.dataset.BUSI_dataloader import BUSI_dataloader_CV_prod
 from src.utils.metrics import dice_score_from_tensor
 from src.utils.miscellany import init_log
 from src.utils.miscellany import seed_everything
@@ -40,7 +40,7 @@ def train_one_epoch():
         # Compute the loss and its gradients
         if type(outputs) == list:
             # if deep supervision
-            loss = torch.sum(torch.stack([loss_fn(o, masks) / (n + 1) for n, o in enumerate(reversed(outputs))]))
+            loss = torch.sum(torch.stack([loss_fn(o, masks) / (j + 1) for j, o in enumerate(reversed(outputs))]))
         else:
             loss = loss_fn(outputs, masks)
         if not torch.isnan(loss):
@@ -66,20 +66,24 @@ def train_one_epoch():
 
 
 # initializing folder structures and log
+start_time = time.perf_counter()
 timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-Path(f"runs/{timestamp}/").mkdir(parents=True, exist_ok=True)
-
 # loading config file
 with open('./src/config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
-    logging.info(pformat(config))
-shutil.copyfile('./src/config.yaml', f'./runs/{timestamp}/config.yaml')
 
 config_model = config['model']
 config_opt = config['optimizer']
 config_loss = config['loss']
 config_training = config['training']
 config_data = config['data']
+
+run_path = f"{timestamp}_{config_model['architecture']}_{config_model['width']}_batch_{config_data['batch_size']}_" \
+           f"{'_'.join(config_data['classes'])}_prod"
+Path(f"runs/{run_path}/").mkdir(parents=True, exist_ok=True)
+logging.info(pformat(config))
+shutil.copyfile('./src/config.yaml', f'./runs/{run_path}/config.yaml')
+
 
 # initializing seed and gpu if possible
 seed_everything(config_training['seed'], cuda_benchmark=config_training['cuda_benchmark'])
@@ -95,7 +99,7 @@ n_augments = sum([v for k, v in config_data['augmentation'].items()])
 transforms = torch.nn.Sequential(
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomVerticalFlip(p=0.5),
-    transforms.RandomRotation(degrees=np.random.choice(range(0, 360))),
+    transforms.RandomRotation(degrees=np.random.choice(range(0, 360)))
 )
 
 if config_training['CV'] > 1:
@@ -117,16 +121,15 @@ for n, (training_loader, test_loader) in enumerate(zip(train_loaders, test_loade
 
     # creating specific paths and experiment's objects for each fold
     init_time = time.perf_counter()
-    Path(f"runs/{timestamp}/fold_{n}/segs/").mkdir(parents=True, exist_ok=True)
-    init_log(log_name=f"./runs/{timestamp}/fold_{n}/execution_fold_{n}.log")
+    Path(f"runs/{run_path}/fold_{n}/segs/").mkdir(parents=True, exist_ok=True)
+    init_log(log_name=f"./runs/{run_path}/fold_{n}/execution_fold_{n}.log")
     model = init_segmentation_model(architecture=config_model['architecture'],
                                     sequences=config_model['sequences'] + n_augments,
                                     width=config_model['width'], deep_supervision=config_model['deep_supervision'],
-                                    save_folder=Path(f'./runs/{timestamp}/')).to(dev)
+                                    save_folder=Path(f'./runs/{run_path}/')).to(dev)
     optimizer = init_optimizer(model=model, optimizer=config_opt['opt'], learning_rate=config_opt['lr'])
     loss_fn = init_loss_function(loss_function=config_loss['function'])
-    # scheduler = CosineAnnealingLR(optimizer, T_max=20, eta_min=1e-6)
-    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, min_lr=1e-6, verbose=True)
+    scheduler = CosineAnnealingLR(optimizer, T_max=20, eta_min=1e-6)
     logging.info(f"\n\n *********************  FOLD {n}  ********************* \n\n")
 
     best_validation_loss = 1_000_000.
@@ -139,45 +142,44 @@ for n, (training_loader, test_loader) in enumerate(zip(train_loaders, test_loade
 
         # Update the learning rate at the end of each epoch
         scheduler.step(avg_train_loss)
-        best_validation_loss = avg_train_loss
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'scheduler': 'scheduler',
-            'val_loss': best_validation_loss
-        }, f'runs/{timestamp}/fold_{n}/model_{timestamp}_fold_{n}')
+        # best_validation_loss = avg_train_loss
 
-        # Iterating over test loader
-        running_test_dice = 0.
-        for k, data in enumerate(test_loader):
-            inputs, masks = data['image'].to(dev), data['mask'].to(dev)
-            outputs = model(inputs)
-            # measuring DICE
-            if type(outputs) == list:
-                outputs = outputs[-1]
-            outputs = torch.sigmoid(outputs) > .5
-            dice = dice_score_from_tensor(masks, outputs)
-            running_test_dice += dice
-        avg_test_dice = running_test_dice / test_loader.__len__()
+        # # Iterating over test loader
+        # running_test_dice = 0.
+        # for k, data in enumerate(test_loader):
+        #     inputs, masks = data['image'].to(dev), data['mask'].to(dev)
+        #     outputs = model(inputs)
+        #     # measuring DICE
+        #     if type(outputs) == list:
+        #         outputs = outputs[-1]
+        #     outputs = torch.sigmoid(outputs) > .5
+        #     dice = dice_score_from_tensor(masks, outputs)
+        #     running_test_dice += dice
+        # avg_test_dice = running_test_dice / test_loader.__len__()
 
         # logging results of current epoch
         end_epoch_time = time.perf_counter()
         logging.info(f'EPOCH {epoch} --> '
                      f'|| Training loss {avg_train_loss:.4f} '
                      f'|| Training DICE {avg_dice:.4f} '
-                     f'|| Test DICE {avg_test_dice:.4f} '
+                     # f'|| Test DICE {avg_test_dice:.4f} '
                      f'|| Epoch time: {end_epoch_time - start_epoch_time:.4f}')
 
+    # Saving model
+    torch.save({
+        'epoch': config_training['epochs'],
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'scheduler': 'scheduler',
+        'val_loss': best_validation_loss
+    }, f'runs/{run_path}/fold_{n}/model_{timestamp}_fold_{n}')
+
     logging.info(f"\nTesting phase for fold {n}")
-    model = load_pretrained_model(model, f'runs/{timestamp}/fold_{n}/model_{timestamp}_fold_{n}')
-    results = inference_segmentation(model=model, test_loader=test_loader, path=f"runs/{timestamp}/fold_{n}/", device=dev)
-
+    model = load_pretrained_model(model, f'runs/{run_path}/fold_{n}/model_{timestamp}_fold_{n}')
+    results = inference_segmentation(model=model, test_loader=test_loader, path=f"runs/{run_path}/fold_{n}/",
+                                     device=dev, threshold_postprocessing=config_training['threshold_postprocessing'])
     logging.info(results)
-
     logging.info(results.mean())
-    logging.info(results.median())
-    logging.info(results.max())
 
     end_time = time.perf_counter()
     logging.info(f"Total time for fold {n}: {end_time - init_time:.2f}")
@@ -187,4 +189,4 @@ for n, (training_loader, test_loader) in enumerate(zip(train_loaders, test_loade
 
 # Measuring total time
 end_time = time.perf_counter()
-logging.info(f"Total time for all of the folds: {end_time - init_time:.2f}")
+logging.info(f"Total time for all of the folds: {end_time - start_time:.2f}")
